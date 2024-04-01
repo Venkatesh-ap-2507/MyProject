a29a8b9bfe1b9a017ae8938ac0621e5eaf03d9e2
venkatesh <venkateshpensalwar561@gmail.com>
2024-04-01 14:18:44 +0530

    All htmls files

--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -5,13 +5,13 @@ from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
 def register(request):
-    if request.metod == "POST":
-        username = request.POST.get("username")
-        email = request.POST.get('email')
-        password = request.POST.get("password")
+    # if request.metod == "POST":
+        # username = request.POST.get("username")
+        # email = request.POST.get('email')
+        # password = request.POST.get("password")
 
-        user = User.objects.create_user(username=username, password=password, email=email)  
-        return render(request,"success.html")
+        # user = User.objects.create_user(username=username, password=password, email=email)  
+        # return render(request,"success.html")
     return render(request,'register.html')
 
 def login(request):
