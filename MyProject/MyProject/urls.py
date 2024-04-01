46e52418201c4aeec24455015beb11d7909d94b1
venkatesh <venkateshpensalwar561@gmail.com>
2024-04-01 14:21:44 +0530

    added home login and register html pages

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from MyApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.register,name="register")
+    path("",views.register,name="register"),
+    path("login/",views.login,name="login"),
 ]
