6e0eb5669fb701970484f6cc71557dcf6618e9c7
venkatesh <venkateshpensalwar561@gmail.com>
2024-04-01 13:32:01 +0530

    First view for login and register

--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
+
 
 # Create your views here.
+def register(request):
+    if request.metod == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get('email')
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(username=username, password=password, email=email)
+        return render(request,"success.html")
+    return render(request,'register.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user =  authenticate(request,username=username, password=password)
+        if user is not None:
+            auth_login(request,user)
+            return render(request,"login_success.html")
+        else:
+            error_message = "Invalid username or password"
+            return render(request,"login.html",{'error':error_message})
+    return render(request,"login.html")
